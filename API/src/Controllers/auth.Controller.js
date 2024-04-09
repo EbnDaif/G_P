4b0428585043a11d34d44482254a94a51fbc6a11
EbnDaif <fadodaif@gmail.com>
2024-04-09 07:23:25 +0200

    Add Logs

--- API/src/Controllers/auth.Controller.js
+++ API/src/Controllers/auth.Controller.js
@@ -1,6 +1,8 @@
 const User = require("../models/User.model");
 const asynchandler = require("express-async-handler");
 const ApiError = require("../utils/apiError");
+const loggerEvent = require('../services/logger.services')
+const logger = loggerEvent('auth')
 exports.NewUser = asynchandler(async (req, res) => {
   if (!req.body.keywords) {
     req.body.keywords = [
@@ -12,19 +14,26 @@ exports.NewUser = asynchandler(async (req, res) => {
     ];
   }
       const data = req.body;
+      logger.info(req.body)
+
      let dublicatedemail = await User.findOne({ email: data.email });
   console.log(req.body);
   if (dublicatedemail) {
+    logger.error("email is already taken ");
+
     return next(new ApiError("this email is already taken", 403));
 
   }
   const newuser = new User(data);
   await newuser.save();
+  logger.info("created email")
   res.status(200).json({ message: "Created" });
 });
 (
   exports.login = asynchandler(async (req, res) => {
     try {
+      logger.info(req.body)
+
       const user = await User.findByCredentials(req.body.email, req.body.password); 
 
       const token = await user.generatetokens();
@@ -36,6 +45,8 @@ exports.NewUser = asynchandler(async (req, res) => {
       });
       res.status(200).send({ user, token });
     } catch (error) {
+      logger.error(error.message);
+
       res.status(401).send({ error: 'Invalid credentials' });
     }
   }))
--- API/src/utils/apiError.js
+++ API/src/utils/apiError.js
@@ -1,5 +1,9 @@
+const loggerEvent = require('../services/logger.services')
+const logger = loggerEvent('API error')
 class ApiError extends Error {
     constructor(message, statuscode) {
+        logger.error(message)
+
         super(message)
         this.statuscode = statuscode
         this.status=`${statuscode}`.startsWith(4)?'fail':'error';
