31eff99319b81e4e6598c80dfbc7d8649be86771
EbnDaif <51683663+EbnDaif@users.noreply.github.com>
2024-04-22 20:37:28 +0200

    nodemailer

---
+++ API/src/utils/nodemailer.js
@@ -0,0 +1,22 @@
+
+const nodemailer= require("nodemailer")
+
+export const sendEmail = async ({ to, subject, html, attachments }) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.Email,
+      pass: process.env.EmailPassword,
+    },
+  });
+
+  const emailInfo = await transporter.sendMail({
+    from: `Nafsi mental health <${process.env.Email}>`,
+    to,
+    subject,
+    html,
+    attachments,
+  });
+
+  return emailInfo.accepted.length < 1 ? false : true;
+};
