62b7be33a9c5bc2669b11351a0b80139f72cd4c7
EbnDaif <fadodaif@gmail.com>
2024-06-14 18:53:45 +0300

    55

--- API/src/Controllers/User.Controller.js
+++ API/src/Controllers/User.Controller.js
@@ -70,7 +70,6 @@ exports.forgetPassword = asyncHandler(async (req, res, next) => {
   if (!user) {
     return next(new ApiError("User not found", 404));
   }
-  console.log(process.env);
   const token = jwt.sign({ _id: user._id }, process.env.RESET_PASSWORD_SALT, {
     expiresIn: "10m",
   });
