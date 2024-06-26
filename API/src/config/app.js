29f2b1495de6f3e35675fecfad9ab7823dfc3c7f
EbnDaif <fadodaif@gmail.com>
2024-06-26 05:03:07 +0300

    0.8.9B

--- API/src/config/app.js
+++ API/src/config/app.js
@@ -3,16 +3,18 @@ const app = express();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
+
 const corsOptions = {
   origin: "http://localhost:5173", // Replace with your client URL
-  credentials: true, 
+  credentials: true,
+  optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
 };
-
 
 app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
 app.use("/GP", require("../Routers/Routers"));
+app.options("*", cors(corsOptions));
 
 // Error handler middleware (must be placed at the end)
 const errorHandler = (err, req, res, next) => {
